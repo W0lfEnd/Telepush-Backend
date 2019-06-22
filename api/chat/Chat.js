50c31878bf820dba49becee362dc1071b81ce224
W0lfEnd <oliinyk.dev@gmail.com>
2019-06-22 13:58:15 +0300

    Changes in model structure and editing routes methods

--- api/chat/Chat.js
+++ api/chat/Chat.js
@@ -1,14 +1,20 @@
 let mongoose = require('mongoose');
 let ChatSchema = new mongoose.Schema({
-    photo_url: String,
-    users_id: [String],
-    messages: [{
-        sender_id: { type: String, index: true },
-        message_text: String,
-        sending_date: { type: Date, index: true }
-    }]
+    photo_url: {data: Buffer, contentType: String, default: ''},
+    users_id: {type: [String], default: []},
+    messages: {
+        type: [{
+            sender_id: { type: String, index: true },
+            message_text: String,
+            sending_date: { type: Date, index: true }
+        }], default: []
+    },
+    is_private: Boolean
 });
 
 mongoose.model('Chat', ChatSchema);
 let Model = mongoose.model('Chat');
-module.exports = Model;
+module.exports =  {
+    Model: Model,
+    MainProjection: {_id:1, photo_url: 1, messages: {$slice: -1}, is_private: 1}
+};
--- api/chat/ChatController.js
+++ api/chat/ChatController.js
@@ -7,21 +7,114 @@ var VerifyToken = require(__root + 'middleware/auth/VerifyToken');
 router.use(bodyParser.urlencoded({ extended: true }));
 var User = require('../users/User');
 var Chat = require('./Chat');
-// RETURNS ALL THE USERS IN THE DATABASE
-router.get('/:id', VerifyToken, function (req, res) {
-    Chat.findById(req.params.id,function (err, chat) {
-        if(err) return res.send(500);
-        if(!chat) return res.send(404);
+
+//get all chats for current user
+router.get('/', VerifyToken, function (req, res) {
+    Chat.Model.find(
+        { users_id: global["currentUser"]._id },
+        Chat.MainProjection,
+        function (err, chats) {
+        if(err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        if(!chats) return res.sendStatus(404);
+        res.send(chats);
     });
 });
 
-// GETS A SINGLE USER FROM THE DATABASE
-router.post('/:id', VerifyToken, function (req, res) {
-    User.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the users.");
-        if (!user) return res.status(404).send("No users found.");
-        res.status(200).send(user);
+//get chat main info
+router.get('/:id', VerifyToken, function (req, res) {
+    Chat.Model.findById(
+        req.params.id,
+        Chat.MainProjection,
+        function (err, chat) {
+        if(err) return res.sendStatus(500);
+        if(!chat) return res.sendStatus(404);
+        res.send(chat);
     });
+});
+
+//get messages from chat
+router.get('/:id/messages', VerifyToken, function (req, res) {
+    if(!req.body.skip || !req.body.count) return res.sendStatus(400);
+    let skip = Number(req.body.skip);
+    let count = Number(req.body.count);
+    console.log({skip,count});
+    Chat.Model.findById(
+        req.params.id,
+        {
+            messages: {$slice: [skip, count] }
+        },
+        function (err, chat) {
+        if(err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        if(!chat) return res.sendStatus(404);
+        res.send(chat.messages);
+    });
+});
+
+
+//get users from chat
+router.get('/:id/users', VerifyToken, function (req, res) {
+    Chat.Model.findById(
+        req.params.id,
+        {
+            _id:1,
+            users_id: 1
+        },
+        function (err, chat) {
+            if(err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            if(!chat) return res.sendStatus(404);
+            res.send(chat.users_id);
+        });
+});
+
+
+//TODO get messages which have date sending more then in request
+router.get('/:id/messages/:date', VerifyToken, function (req, res) {
+    let date = req.params.date;
+    Chat.Model.findById(
+        req.params.id,
+        {
+            _id:1,
+            "messages.date_sending" : {$gt: new Date(date)}
+        },
+        function (err, chat) {
+            if(err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            if(!chat) return res.sendStatus(404);
+            res.send(chat.messages);
+    });
+});
+
+//send message to chat
+router.post('/:id/send', VerifyToken, function (req, res) {
+    if(!req.body.message) return res.sendStatus(400);
+    Chat.Model.findById(req.params.id,{},function (err, doc) {
+        if(err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        if(!doc.users_id.includes(global["currentUser"]._id)) return res.sendStatus(403);
+        Chat.Model.findOneAndUpdate(
+            req.params.id,
+            {$addToSet: { messages: req.body.message}},
+            function (err, chat) {
+                if(err) {
+                    console.log(err);
+                    return res.sendStatus(500);
+                }
+                res.sendStatus(200);
+            });
+    })
 });
 
 
